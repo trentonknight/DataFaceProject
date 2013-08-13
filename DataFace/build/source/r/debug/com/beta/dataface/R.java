08884e62cdac173095c90cde366150a7a2080010
trentonknight <trentonknight@gmail.com>
2013-08-13 06:26:43 -0400

    mainactivity updated.....

--- DataFace/build/source/r/debug/com/beta/dataface/R.java
+++ DataFace/build/source/r/debug/com/beta/dataface/R.java
@@ -27,22 +27,23 @@ public final class R {
         public static final int view_as_list=0x7f020006;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000c;
-        public static final int button=0x7f080005;
-        public static final int content_add=0x7f08000d;
-        public static final int content_discard=0x7f08000f;
-        public static final int cube_txt=0x7f08000b;
-        public static final int cubename_txt=0x7f08000a;
-        public static final int editText=0x7f080004;
-        public static final int editText2=0x7f080006;
+        public static final int action_settings=0x7f08000d;
+        public static final int button=0x7f080008;
+        public static final int content_add=0x7f08000e;
+        public static final int content_discard=0x7f080010;
+        public static final int cube_txt=0x7f08000c;
+        public static final int cubename_txt=0x7f08000b;
+        public static final int editText=0x7f080007;
+        public static final int editText2=0x7f080009;
         public static final int imageView=0x7f080000;
-        public static final int obcontent_enter=0x7f080002;
-        public static final int object_name=0x7f080003;
-        public static final int obname_enter=0x7f080001;
-        public static final int parent_txt=0x7f080009;
-        public static final int textView=0x7f080008;
-        public static final int textView2=0x7f080007;
-        public static final int view_as_list=0x7f08000e;
+        public static final int obcontent_enter=0x7f080005;
+        public static final int object_name=0x7f080006;
+        public static final int obname_enter=0x7f080004;
+        public static final int parent_txt=0x7f08000a;
+        public static final int textView=0x7f080001;
+        public static final int textView2=0x7f080002;
+        public static final int textView3=0x7f080003;
+        public static final int view_as_list=0x7f08000f;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
